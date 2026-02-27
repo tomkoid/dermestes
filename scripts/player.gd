cb33e703a02d910384b1b5f6d7213d7175c6301b
Tomkoid <tomkoid@tomkoid.cz>
2026-02-27 23:16:12 +0100

    feat: smooth beetle rotation
    Signed-off-by: Tomkoid <tomkoid@tomkoid.cz>

--- scripts/player.gd
+++ scripts/player.gd
@@ -42,13 +42,15 @@ func _process(delta: float) -> void:
 	#_update_visuals()
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var dir := Vector2(
 		Input.get_axis("ui_left", "ui_right"),
 		Input.get_axis("ui_up", "ui_down")
 	)
 	if dir != Vector2.ZERO:
 		_ass.play("running")
+		var target_angle = dir.angle() + PI/2
+		rotation = lerp_angle(rotation, target_angle, 0.15)
 		velocity = dir.normalized() * speed
 	else:
 		_ass.play("idle")
