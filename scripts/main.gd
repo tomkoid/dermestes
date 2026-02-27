1b302139cdd66170cecb664517a0c7442c1cb130
Tomkoid <tomkoid@tomkoid.cz>
2026-02-28 00:20:42 +0100

    feat: enemy spawner, enemy1 spritesheet
    Signed-off-by: Tomkoid <tomkoid@tomkoid.cz>

---
+++ scripts/main.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+@onready var enemy = preload("res://scenes/enemy.tscn")
+@onready var player_ref: CharacterBody2D = $"../Beetle"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#var enemy_spawn_timer = get_tree().create_timer(2, true, false, true)
+	var enemy_spawn_timer = Timer.new()
+	enemy_spawn_timer.name = "EnemySpawnTimer"
+	enemy_spawn_timer.autostart = true
+	enemy_spawn_timer.one_shot = false
+	add_child(enemy_spawn_timer)
+	
+	enemy_spawn_timer.timeout.connect(_spawn_enemy)
+
+func _spawn_enemy():
+	var e = enemy.instantiate()
+	$Enemies.add_child(e)
