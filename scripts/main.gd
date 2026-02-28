1be7c5dbbc97995ec4578fbc35e5e2286d674a5e
Tomkoid <tomkoid@tomkoid.cz>
2026-02-28 09:30:55 +0100

    fix: coffins eaten by enemies also get deleted, centralize grave consumed logic
    Signed-off-by: Tomkoid <tomkoid@tomkoid.cz>

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 <<<<<<< HEAD
 	_grid.grave_consumed.connect(_on_body_consumed)
 =======
-	player_ref.body_consumed.connect(_on_body_consumed)
+	_grid.grave_consumed.connect(_on_body_consumed)
 >>>>>>> 96ccb59 (feat: remove coffins from map if empty)
 
 	#var enemy_spawn_timer = get_tree().create_timer(2, true, false, true)
