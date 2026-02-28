098cee578b55aa4e30419675b986827f288528aa
grreby <grreby@proton.me>
2026-02-28 15:19:05 +0100

    add ui

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -9,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn") # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/settings.tscn") # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit() # Replace with function body.
