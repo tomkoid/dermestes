9e658acca522bef4648ed2e054d5cd20dd20fe4c
grreby <grreby@proton.me>
2026-03-01 05:58:30 +0100

    add global button sound

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -2,3 +2,23 @@ extends Node
 
 var graves_eaten: int = 0
 var time_elapsed: float = 0.0
+
+var _player: AudioStreamPlayer
+
+
+func _ready() -> void:
+	_player = AudioStreamPlayer.new()
+	_player.bus = "Master"
+	_player.stream = preload("res://assets/sounds/button.mp3")
+	add_child(_player)
+	get_tree().node_added.connect(_on_node_added)
+
+
+func _on_node_added(node: Node) -> void:
+	if node is Button:
+		node.pressed.connect(_play_click)
+
+
+func _play_click() -> void:
+	if _player.stream:
+		_player.play()
