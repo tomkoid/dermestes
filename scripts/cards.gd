2ac6f817fe0acfdc05729076c9b61c1a9d59b267
grreby <grreby@proton.me>
2026-03-01 03:57:26 +0100

    cards changes

--- scripts/cards.gd
+++ scripts/cards.gd
@@ -9,8 +9,8 @@ var CARDS: Array[Card] = [
 	card_initializer.init("Dr. House", "+30 HP", "Není to Lupus!!", Card.CardType.INTELLECT, "res://assets/karty/house.png"),
 	card_initializer.init("Nathan Drake", "+30 HP", "", Card.CardType.INTELLECT, "res://assets/karty/nathan.png"),
 	card_initializer.init("Flash", "+30 HP", "", Card.CardType.INTELLECT, "res://assets/karty/flash.png"),
-	card_initializer.init("Jindřich ze Skalice", "Shield", "I feel quite hungry.", Card.CardType.DEFENSE, "res://assets/karty/jindra.png"),
-	card_initializer.init("Cpt. America", "Shield", "Prohra Občanské války s Iron Manem nedopadlo úplně podle jeho představ", Card.CardType.DEFENSE, "res://assets/karty/cptamerica.png"),
+	card_initializer.init("Henry of Skalitz", "Shield", "I feel quite hungry.", Card.CardType.DEFENSE, "res://assets/karty/jindra.png"),
+	card_initializer.init("Cpt. America", "Shield", "Prohra Občanské války s Iron Manem nedopadla úplně podle jeho představ", Card.CardType.DEFENSE, "res://assets/karty/cptamerica.png"),
 	card_initializer.init("Gandalf", "Shield", "You shall not pass", Card.CardType.DEFENSE, "res://assets/karty/gandalf.png"),
 ]
 
